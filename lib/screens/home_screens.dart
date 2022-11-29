72241fd55b04eb4bd79dbc0ff708335ff72375c5
Shakriya Panday <fenom.shakriyapanday@gmail.com>
2022-11-29 23:48:31 +0545

    Categories expense added

--- lib/screens/home_screens.dart
+++ lib/screens/home_screens.dart
@@ -1,4 +1,5 @@
 import 'package:budget_app_ui/data/data.dart';
+import 'package:budget_app_ui/helpers/color_helper.dart';
 import 'package:budget_app_ui/models/category_model.dart';
 import 'package:budget_app_ui/widgets/bar_char.dart';
 import 'package:flutter/material.dart';
@@ -14,6 +15,7 @@ class _HomeScreenState extends State<HomeScreen> {
   _buildCategory(Category category, double totalAmountSpend) {
     return Container(
       margin: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+      padding: const EdgeInsets.all(20.0),
       height: 100.0,
       width: double.infinity,
       decoration: BoxDecoration(
@@ -37,6 +39,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 style: const TextStyle(
                   fontSize: 20.0,
                   fontWeight: FontWeight.w600,
+                  letterSpacing: -1,
                 ),
               ),
               Text(
@@ -44,10 +47,42 @@ class _HomeScreenState extends State<HomeScreen> {
                 style: const TextStyle(
                   fontSize: 18.0,
                   fontWeight: FontWeight.w600,
+                  letterSpacing: -0.8,
                 ),
               ),
             ],
-          )
+          ),
+          const SizedBox(
+            height: 10.0,
+          ),
+          LayoutBuilder(
+            builder: (BuildContext context, BoxConstraints constraints) {
+              final double maxBarWidth = constraints.maxWidth;
+              final double percent =
+                  (category.maxAmount - totalAmountSpend) / category.maxAmount;
+              double barWidth = maxBarWidth * percent;
+              if (barWidth < 0) {
+                barWidth = 0;
+              }
+              return Stack(
+                children: [
+                  Container(
+                    height: 20.0,
+                    decoration: BoxDecoration(
+                        color: Colors.grey[200],
+                        borderRadius: BorderRadius.circular(15.0)),
+                  ),
+                  Container(
+                    height: 20.0,
+                    width: barWidth,
+                    decoration: BoxDecoration(
+                        color: getColor(context, percent),
+                        borderRadius: BorderRadius.circular(15.0)),
+                  )
+                ],
+              );
+            },
+          ),
         ],
       ),
     );
