3e43c53721f593ea833ac2c92e1eddf811ca88a1
Shakriya Panday <fenom.shakriyapanday@gmail.com>
2022-11-30 20:58:46 +0545

    percentage indicator added

--- lib/screens/catergory_screen.dart
+++ lib/screens/catergory_screen.dart
@@ -1,5 +1,8 @@
+import 'package:budget_app_ui/helpers/color_helper.dart';
 import 'package:budget_app_ui/models/category_model.dart';
+import 'package:budget_app_ui/models/expense_model.dart';
 import 'package:flutter/material.dart';
+import 'package:percent_indicator/percent_indicator.dart';
 
 class CategoryScreen extends StatefulWidget {
   final Category category;
@@ -13,6 +16,12 @@ class CategoryScreen extends StatefulWidget {
 class _CategoryScreenState extends State<CategoryScreen> {
   @override
   Widget build(BuildContext context) {
+    double totalAmountSpent = 0;
+    widget.category.expenses.forEach(((Expense expense) {
+      totalAmountSpent += expense.cost;
+    }));
+    final double totalAmountLeft = widget.category.maxAmount - totalAmountSpent;
+    final double percent = totalAmountLeft / widget.category.maxAmount;
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -23,10 +32,49 @@ class _CategoryScreenState extends State<CategoryScreen> {
             icon: const Icon(
               Icons.add,
               size: 30.0,
+              shadows: [BoxShadow()],
             ),
           )
         ],
       ),
+      body: SingleChildScrollView(
+          child: Column(
+        children: [
+          Container(
+            margin: const EdgeInsets.all(20.0),
+            padding: const EdgeInsets.all(20.0),
+            height: 250.0,
+            width: double.infinity,
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10.0),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Colors.black12,
+                    offset: Offset(0, 2),
+                    blurRadius: 6.0,
+                  )
+                ]),
+            child: CircularPercentIndicator(
+              animation: true,
+              animationDuration: 500,
+              radius: 100.0,
+              lineWidth: 20.0,
+              percent: percent,
+              circularStrokeCap: CircularStrokeCap.round,
+              backgroundColor: Colors.grey[200]!,
+              progressColor: getColor(context, percent),
+              center: Text(
+                "\$${totalAmountLeft.toStringAsFixed(2)}/\$${widget.category.maxAmount.toStringAsFixed(2)}",
+                style: const TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+          ),
+        ],
+      )),
     );
   }
 }
